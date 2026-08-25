e4477dcc387cb956edba66638646dbfa6a24ca78
Shahriar Rahi <hamid50-041@diu.edu.bd>
2026-08-25 22:11:41 +0600

    binary serach tree

---
+++ Binary Search tree/Binary Search/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    internal class Program
+    {
+        class Node
+        {
+            public int Data;
+            public Node left;
+            public Node right;
+
+            public Node(int data)
+            {
+                Data = data;
+                left = null;
+                right = null;
+            }
+        }
+
+        class BinarySearchTree
+        {
+            public Node root;
+
+            public void Insert(int val)
+            {
+                root = InsertNode(root, val); 
+            }
+
+            private Node InsertNode(Node root, int val)
+            {
+                if(root == null)
+                    return new Node(val);
+                if(root.Data > val)
+                    root.left = InsertNode(root.left, val);
+                if(root.Data < val)
+                    root.right = InsertNode(root.right, val);
+                return root;
+            }
+
+            public bool Search(Node root, int val)
+            {
+                if(root == null)
+                   return false;
+                if(root.Data == val)
+                    return true;
+                if(root.Data > val)
+                    return Search(root.left, val);
+                return Search(root.right, val);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+
+            tree.Insert(1);
+            tree.Insert(2);
+            tree.Insert(3);
+            tree.Insert(4);
+            tree.Insert(5);
+            tree.Insert(6);
+
+            Console.WriteLine(tree.Search(tree.root, 9));
+        }
+    }
+}
